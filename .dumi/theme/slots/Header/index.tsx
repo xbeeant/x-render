c961e00fc8b291c96db7b9d115f2064f60cd2eb5
wb-hbc831417 <wb-hbc831417@alibaba-inc.com>
2023-02-10 15:53:12 +0800

    feat: header 样式调整

--- .dumi/theme/slots/Header/index.tsx
+++ .dumi/theme/slots/Header/index.tsx
@@ -1,45 +1,38 @@
 import React from 'react';
 import DumiLogo from 'dumi/theme-default/slots/Logo';
 import DumiSearch from 'dumi/theme-default/slots/SearchBar';
-import { Link } from 'dumi';
+import { Row, Col, Space, Divider } from 'antd';
+import Navigation from './Navigation';
+import ThemeSwitch from './ThemeSwitch';
+import GithubLink from './GithubLink';
 
 import './index.less';
 
 const prefix = 'xr-doc-header';
 
-const navs = [
-  {
-    title: 'FormRender',
-    link: '/form-render',
-  },
-  {
-    title: 'TableRender',
-    link: '/table-render',
-  },
-  {
-    title: 'ChartRender',
-    link: '/chart-render',
-  },
-]
-
 const Header: React.FC = () => {
   return (
-    <div className={prefix}>
-      <div className={`${prefix}-content`}>
-        <div className={`${prefix}-left`}>
-          <DumiLogo />
-          <DumiSearch />
-        </div>
-        <div className={`${prefix}-right`}>
-          <div className={`${prefix}-nav`}>
-            {navs.map(i => (
-              <Link key={i.title} to={i.link}>{i.title}</Link>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <header className={prefix}>
+      <Row align="middle" justify="space-between" style={{ padding: '10px 0' }}>
+        <Col xl={{ push: 2 }} xxl={{ push: 3 }} push={1}>
+          <Space size={20}>
+            <DumiLogo />
+            <DumiSearch />
+          </Space>
+        </Col>
+        <Col xl={{ pull: 2 }} xxl={{ pull: 3 }} pull={1}>
+          <Space size={0}>
+            <Navigation />
+            <Divider type="vertical" style={{ marginLeft: 0 }} />
+            <Space>
+              <ThemeSwitch />
+              <GithubLink />
+            </Space>
+          </Space>
+        </Col>
+      </Row>
+    </header>
+  );
+};
 
 export default Header;
